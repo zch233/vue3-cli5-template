db11376c5c07b24e327c03dbe247c5dca390814a
zhangchao <529743595@qq.com>
2022-01-07 14:16:50 +0800

    chore: update alias @ => @src

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require('path');
+
+const resolve = dir => path.join(__dirname, '.', dir);
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@src': resolve('src'),
+      },
+    },
+  },
 });

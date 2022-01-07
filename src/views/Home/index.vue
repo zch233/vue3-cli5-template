be36b19855dc111cacc1996d564cd41c6ff2f10e
zhangchao <529743595@qq.com>
2022-01-07 14:50:35 +0800

    feat: clean router files

---
+++ src/views/Home/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" :src="require('@src/assets/logo.png')" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from '@src/components/HelloWorld.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        HelloWorld,
+    },
+    mounted() {
+        console.log(11111, this.a?.s);
+    },
+};
+</script>

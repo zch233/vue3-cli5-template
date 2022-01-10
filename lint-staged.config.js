93e244ab48ac1ef06d72d882ea4efa4fe156842f
zhangchao <529743595@qq.com>
2022-01-10 15:09:41 +0800

    chore: add lint stylelint when git commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    '*.{js,jsx,vue}': 'vue-cli-service lint',
+    '*.{js,jsx,vue,tsx}': 'vue-cli-service lint',
+    '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
+    '*.{scss,less,styl,css,vue}': ['stylelint --fix'],
 };

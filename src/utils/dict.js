8ba104cc923f9f8d77a80eda2517e071cee9d0c0
zhangchao <529743595@qq.com>
2022-01-11 16:52:24 +0800

    feat: add dict

---
+++ src/utils/dict.js
@@ -0,0 +1,19 @@
+export const GENDER = {
+    UNKNOWN: '0',
+    MAN: '1',
+    FEMALE: '2',
+    label: {
+        0: '未知',
+        1: '男',
+        2: '女',
+    },
+};
+
+export const AUTH_ID_CARD = {
+    UN_AUTH: '0',
+    AUTH: '1',
+    label: {
+        0: '未认证',
+        1: '已认证',
+    },
+};

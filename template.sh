b283029635a0fa8b9cfd5a3a42421fb646e3004e
zhangchao <529743595@qq.com>
2022-03-02 22:20:40 +0800

    feat: update template bash

--- template.sh
+++ template.sh
@@ -2,10 +2,12 @@
 
 # 确保脚本抛出遇到的错误
 set -e
+
 tempRemoteName="temp2333333"
+
+git pull &&
 git remote add $tempRemoteName $1 &&
 git push $tempRemoteName master &&
 git remote remove $tempRemoteName &&
 cd .. &&
-git clone $1 &&
-yarn
+git clone $1

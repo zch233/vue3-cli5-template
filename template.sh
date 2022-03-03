20aea549be2671653ea4f94a0023c2697f243040
zhangchao <529743595@qq.com>
2022-03-03 14:30:08 +0800

    feat: update template.sh

--- template.sh
+++ template.sh
@@ -4,10 +4,21 @@
 set -e
 
 tempRemoteName="temp2333333"
+gitUrl=$1
+urlArray=(${gitUrl//// })
+urlArrayLastIndex=$((${#urlArray[@]}-1))
+lastString=${urlArray[urlArrayLastIndex]}
+lastStringArray=(${lastString//./ })
+dirName=${lastStringArray[0]}
 
 git pull &&
-git remote add $tempRemoteName $1 &&
+git remote add $tempRemoteName $gitUrl &&
 git push $tempRemoteName master &&
 git remote remove $tempRemoteName &&
 cd .. &&
-git clone $1
+git clone $1 &&
+cd $dirName &&
+start . &&
+code . &&
+yarn &&
+echo "恭喜你，创建成功"

4252106008489f8954bc75dce8a5214b33bcbdc0
zhangchao <529743595@qq.com>
2022-03-08 18:03:16 +0800

    feat: add reset css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 import { store } from './store';
+import '@src/styles/reset.css';
 import '@src/utils/svg';
 import '@src/styles/global.less';
 import 'nprogress/nprogress.css';

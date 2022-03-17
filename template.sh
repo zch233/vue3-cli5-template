473d46370fda1cd78843a34bc28b6d81472c2d7e
zhangchao <529743595@qq.com>
2022-03-17 15:31:52 +0800

    chore: update template.sh because codeup rules change

--- template.sh
+++ template.sh
@@ -2,8 +2,10 @@
 
 # 确保脚本抛出遇到的错误
 set -e
+branchName="feature/`date +%Y-%m-%d`"
+tempRemoteName="template/`date +%Y-%m-%d-%H-%m-%s`"
+currentBranch=`git symbolic-ref --short -q HEAD`
 
-tempRemoteName="temp2333333"
 gitUrl=$1
 urlArray=(${gitUrl//// })
 urlArrayLastIndex=$((${#urlArray[@]}-1))
@@ -13,11 +15,12 @@ dirName=${lastStringArray[0]}
 
 git pull &&
 git remote add $tempRemoteName $gitUrl &&
-git push $tempRemoteName master &&
+git push $tempRemoteName $currentBranch:$branchName &&
 git remote remove $tempRemoteName &&
 cd .. &&
 git clone $1 &&
 cd $dirName &&
+git checkout -b $branchName origin/$branchName
 start . &&
 code . &&
 yarn &&

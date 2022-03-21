51c66b218d61621be569df47fec6a45d63e7ec47
zhangchao <529743595@qq.com>
2022-03-21 15:14:50 +0800

    feat: import global less

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 import { store } from './store';
-import '@src/styles/reset.css';
 import '@src/utils/svg';
+import '@src/styles/reset.css';
+import '@src/styles/global.less';
 
 createApp(App).use(store).use(router).mount('#app');
